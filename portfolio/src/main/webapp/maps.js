317a7eb1c0bb18e45e54b25018fe8cb8278282e2
Ryan Simone <ryansimone2324@gmail.com>
2020-07-17 00:08:45 +0000

    Displays the map with markers at each surf spot

--- portfolio/src/main/webapp/maps.js
+++ portfolio/src/main/webapp/maps.js
@@ -1,12 +1,54 @@
 const mapsKey = config.MAPS_KEY;
-
+const surfSpots = ['pv', 'tpz', 'bw', 'lf', 'nmbp', 'porto'];
 window.onload = createScriptTagForMap();
+
 
 function createMap() {
     const map = new google.maps.Map(
         document.getElementById('map-container'),
-        {center: {lat:33.835, lng: -118.390}, zoom: 5});
-    console.log("Map should be created")
+        {center: {lat:33.835, lng: -118.390}, zoom: 12});
+    let pvLatLng = {lat: 33.792, lng: -118.407};
+    let tpzLatLng = {lat: 33.832, lng: -118.392};
+    let bwLatLng = {lat: 33.852, lng: -118.399};
+    let lfLatLng = {lat: 33.875, lng: -118.408};
+    let nmbpLatLng = {lat: 33.884, lng: -118.413};
+    let portoLatLng = {lat: 33.905, lng: -118.423};
+
+    let pvMark = new google.maps.Marker({
+        position: pvLatLng,
+        map: map,
+        title: 'Palos Verdes Cove'
+    });
+
+    let tpzMark = new google.maps.Marker({
+        position: tpzLatLng,
+        map: map,
+        title: 'Topaz'
+    });
+
+    let bwMark = new google.maps.Marker({
+        position: bwLatLng,
+        map: map,
+        title: 'Break Wall'
+    });
+
+    let lfMark = new google.maps.Marker({
+        position: lfLatLng,
+        map: map,
+        title: 'Longfellow'
+    });
+
+    let nmbpMark = new google.maps.Marker({
+        position: nmbpLatLng,
+        map: map,
+        title: 'Northside Manhattan Beach Pier'
+    });
+
+    let portoMark = new google.maps.Marker({
+        position: portoLatLng,
+        map: map,
+        title: 'El Porto'
+    });
 }
 
 function createScriptTagForMap() {
