b85b7102efa4cb111a2d696608b7aad0968c5d0d
Ryan Simone <ryansimone2324@gmail.com>
2020-07-23 18:43:07 +0000

    Implements the query function to pass all the tests

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -15,19 +15,118 @@
 package com.google.sps;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Comparator;
+import java.util.ArrayList;
 
 public final class FindMeetingQuery {
+
+  /*
+   * Given a list of scheduled {@code events}, query() returns a list of all available times for the 
+   * new meeting {@code request}.
+   *
+   * @param events: list of scheduled events for attendees/optional attendees
+   * @param request: the new meeting that is to be scheduled 
+   *
+   * @return: a list of all available times the meeting can be held
+   */
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
-    //create an empty Collection<TimeRange> for available times for the requested meeting
-    //check if the duration of the event is more than a day or if the collection of attendees is empty
-    //if yes, return the empty Collection
-    //if no, begin looking for appropriate time slots
-    //Iterate through the day by checking each appropriate time duration ex. if meeting duration is 1hr 8-9, 8:30-9:30, ...
-    //for each time duration check if there is an event present and if anyone is already attending that meeting
-    //If the time is accepted, add it to the collection
-    //return the collection of times
+    
+    /* Checks if the new meeting {@code request} is longer than a day.
+     * If yes, return an empty list;
+     */
+    if(request.getDuration() > TimeRange.WHOLE_DAY.duration()){
+      return new ArrayList<>();
+    }
 
+    /* Checks if there are optional attendees for the {@code request}
+     * If yes, search for a time where both optional and non-optional attendees can attend.
+     * If no such time exists, priority goes to non-optional attendees
+     */
+    if(request.checkForOptionalAttendees()){
+      ArrayList<TimeRange> availableTimes = findAvailableMeetingTimes(events, request, true);
+      if(!request.checkForAttendees() || !availableTimes.isEmpty()){
+          return availableTimes;
+      }
+    }
+    return findAvailableMeetingTimes(events, request, false);
+  }
 
+  /* Given a list of {@code events}, findAvailableMeetingTimes() searches for all the possible times the new meeting
+   * {@code request} can take place. 
+   * 
+   * @param events: list of scheduled events for attendees/optional attendees
+   * @param request: the new meeting that is to be scheduled
+   * @param hasOptionalAttendees: boolean value that informs the function of whether there are optional attendees for the new meeting
+   *
+   * @return: a sorted list of available meeting times in chronological order
+   */
+  private ArrayList<TimeRange> findAvailableMeetingTimes(Collection<Event> events, MeetingRequest request, boolean hasOptionalAttendees){
+
+    // Creates a list of all the time ranges in a day to check for conflicts with {@code events}
+    ArrayList<TimeRange> availableMeetingTimes = new ArrayList<TimeRange>();
+    availableMeetingTimes.add(TimeRange.WHOLE_DAY);
+
+    // Iterates through all the scheduled events to find any conflicts
+    for(Event event : events){
+      TimeRange eventTime = event.getWhen();
+      
+      /* Checks if the {@ code event} has any attendees that are also an attendee for the new meeting {@code request}
+       * If the two collections are disjoint, skip this event
+       */
+      if((Collections.disjoint(event.getAttendees(), request.getAttendees())) && (!hasOptionalAttendees || Collections.disjoint(event.getAttendees(), request.getOptionalAttendees()))){
+          continue;
+      }
+      // List of all times that the meeting can take place
+      ArrayList<TimeRange> addTimes = new ArrayList<TimeRange>();
+      // List of all the times the meeting cannot take place
+      ArrayList<TimeRange> removeTimes = new ArrayList<TimeRange>();
+
+      for(TimeRange time: availableMeetingTimes){
+          if(time.overlaps(eventTime)) {
+            timesAroundEvent(addTimes, removeTimes, request.getDuration(), time, eventTime);
+          }
+      }
+      availableMeetingTimes.removeAll(removeTimes);
+      availableMeetingTimes.addAll(addTimes);
+      }
+      return sortArray(availableMeetingTimes);
+    }
+  
+  /*
+   * Resolves conflicting event times by checking before and after the {@code eventTime} for
+   * available times with appropriate {@code newMeetingDuration}
+   *
+   * @param addTimes: list of all the available time ranges for the new meeting
+   * @param removeTimes: list of all the time ranges where the meeting cannot be held
+   * @param newMeetingDuration: the duration of requested meeting
+   * @param time: The conflicting time that the function is checking around
+   * @param eventTime: the time of the event 
+   */ 
+  private void timesAroundEvent(ArrayList<TimeRange> addTimes, ArrayList<TimeRange> removeTimes, long newMeetingDuration, TimeRange time, TimeRange eventTime) {
+    TimeRange beforeConflict = TimeRange.fromStartEnd(time.start(), eventTime.start(), false);
+    TimeRange afterConflict = TimeRange.fromStartEnd(eventTime.end(), time.end(), false);
+    if(beforeConflict.duration() >= newMeetingDuration){
+      addTimes.add(beforeConflict);
+    }
+    if(afterConflict.duration() >= newMeetingDuration){
+      addTimes.add(afterConflict);
+    }
+    removeTimes.add(time);
+  }
+
+  /*
+   * Returns a sorted array in chronological order
+   */
+  private ArrayList<TimeRange> sortArray(Collection<TimeRange> arr){
+    ArrayList<TimeRange> sortedArr = new ArrayList<>(arr);
+    Collections.sort(sortedArr, new Comparator<TimeRange>(){
+      @Override
+      public int compare(TimeRange t1, TimeRange t2){
+        return Long.compare(t1.start(), t2.start());
+      }
+    });
+    return sortedArr;
   }
 }
