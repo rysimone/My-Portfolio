023d97d6d4876a29db25f5362b3cba0c0723fd55
Ryan Simone <ryansimone2324@gmail.com>
2020-07-24 18:05:40 +0000

    Updates comments to better reflect functionality

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -41,16 +41,16 @@ public final class FindMeetingQuery {
     }
 
     /* Checks if there are optional attendees for the {@code request}
-     * If yes, search for a time where both optional and non-optional attendees can attend.
+     * If yes, search for times where both optional and non-optional attendees can attend.
      * If no such time exists, priority goes to non-optional attendees
      */
     if(request.checkForOptionalAttendees()){
-      ArrayList<TimeRange> availableTimes = findAvailableMeetingTimes(events, request, true);
+      ArrayList<TimeRange> availableTimes = findAvailableMeetingTimes(events, request, /* optionalAttendees= */true);
       if(!request.checkForAttendees() || !availableTimes.isEmpty()){
           return availableTimes;
       }
     }
-    return findAvailableMeetingTimes(events, request, false);
+    return findAvailableMeetingTimes(events, request, /* optionalAttendees= */false);
   }
 
   /* Given a list of {@code events}, findAvailableMeetingTimes() searches for all the possible times the new meeting
@@ -58,11 +58,11 @@ public final class FindMeetingQuery {
    * 
    * @param events: list of scheduled events for attendees/optional attendees
    * @param request: the new meeting that is to be scheduled
-   * @param hasOptionalAttendees: boolean value that informs the function of whether there are optional attendees for the new meeting
+   * @param optionalAttendees: boolean value that signals whether or not to consider optional attendees from the {@code request}
    *
    * @return: a sorted list of available meeting times in chronological order
    */
-  private ArrayList<TimeRange> findAvailableMeetingTimes(Collection<Event> events, MeetingRequest request, boolean hasOptionalAttendees){
+  private ArrayList<TimeRange> findAvailableMeetingTimes(Collection<Event> events, MeetingRequest request, boolean optionalAttendees){
 
     // Creates a list of all the time ranges in a day to check for conflicts with {@code events}
     ArrayList<TimeRange> availableMeetingTimes = new ArrayList<TimeRange>();
@@ -75,19 +75,20 @@ public final class FindMeetingQuery {
       /* Checks if the {@code event} has any attendees that are also an attendee for the new meeting {@code request}
        * If the two collections are disjoint, skip this event
        */
-      if(checkIfEventsAreDisjoint(event, request, hasOptionalAttendees)){
+      if(checkIfAttendeesAreDisjoint(event, request, optionalAttendees)){
           continue;
       }
-      // List of all times that the meeting can take place
+      // List to hold any newly-created {@link TimeRange}s from {@code timesAroundEvent}
       ArrayList<TimeRange> addTimes = new ArrayList<TimeRange>();
-      // List of all the times the meeting cannot take place
+      // List to hold any newly-created {@link TimeRange}s from {@code timesAroundEvent} in which the meeting cannot be held
       ArrayList<TimeRange> removeTimes = new ArrayList<TimeRange>();
 
       // Checks if the {@code eventTime} overlaps with the current {@code time}
       for(TimeRange time: availableMeetingTimes){
-          if(time.overlaps(eventTime)) {
-            timesAroundEvent(addTimes, removeTimes, request.getDuration(), time, eventTime);
-          }
+        if(time.overlaps(eventTime)) {
+          removeTimes.add(time);
+          addTimes.addAll(timesAroundEvent(request.getDuration(), time, eventTime));
+        }
       }
       availableMeetingTimes.removeAll(removeTimes);
       availableMeetingTimes.addAll(addTimes);
@@ -97,15 +98,17 @@ public final class FindMeetingQuery {
   
   /*
    * Resolves conflicting event times by checking before and after the {@code eventTime} for
-   * available times with appropriate {@code newMeetingDuration}
+   * available times with appropriate {@code newMeetingDuration}. If a new available {@code time} is found, it is added to the {@code newTimes} list.
    *
-   * @param addTimes: list of all the available time ranges for the new meeting
-   * @param removeTimes: list of all the time ranges where the meeting cannot be held
    * @param newMeetingDuration: the duration of requested meeting
-   * @param time: The conflicting time that the function is checking around
-   * @param eventTime: the time of the event 
+   * @param time: The possible new meeting {@code request} time, where an overlap in time was found with a pre-existing event
+   * @param eventTime: the time of the pre-existing event 
+   * 
+   * @return: a {@code newTimes} list that consists of the available times found around the pre-existing event time
    */ 
-  private void timesAroundEvent(ArrayList<TimeRange> addTimes, ArrayList<TimeRange> removeTimes, long newMeetingDuration, TimeRange time, TimeRange eventTime) {
+  private ArrayList<TimeRange> timesAroundEvent(long newMeetingDuration, TimeRange time, TimeRange eventTime) {
+    // Creates a list to store the new available meeting times
+    ArrayList<TimeRange> newTimes = new ArrayList<TimeRange>();
     // Create a Time Range for before the {@code eventTime}
     TimeRange beforeConflict = TimeRange.fromStartEnd(time.start(), eventTime.start(), false);
     // Create a Time Range for after teh {@code eventTime}
@@ -113,12 +116,12 @@ public final class FindMeetingQuery {
 
     // Checks if the new Time Ranges created are longer than the {@code newMeetingDuration}
     if(beforeConflict.duration() >= newMeetingDuration){
-      addTimes.add(beforeConflict);
+      newTimes.add(beforeConflict);
     }
     if(afterConflict.duration() >= newMeetingDuration){
-      addTimes.add(afterConflict);
+      newTimes.add(afterConflict);
     }
-    removeTimes.add(time);
+    return newTimes;
   }
 
   /*
@@ -126,26 +129,20 @@ public final class FindMeetingQuery {
    */
   private ArrayList<TimeRange> sortArray(Collection<TimeRange> arr){
     ArrayList<TimeRange> sortedArr = new ArrayList<>(arr);
-    Collections.sort(sortedArr, new Comparator<TimeRange>(){
-      @Override
-      public int compare(TimeRange t1, TimeRange t2){
-        return Long.compare(t1.start(), t2.start());
-      }
-    });
+    Collections.sort(sortedArr, TimeRange.ORDER_BY_START);
     return sortedArr;
   }
 
   /* Checks if the {@code event} and the {@code request} are disjointed in their attendees and optional attendees
-   * 
+   * Optional attendees are only checked if the {@param hasOptionalAttendees} is true
    * @param events: list of scheduled events
    * @param request: the meeting being requested
    * @param hasOptionalAttendees: true if the {@code request} has optional attendees
    * @return: true if the events are dijointed in attendees, false otherwise
    */
-  private boolean checkIfEventsAreDisjoint(Event event, MeetingRequest request, boolean hasOptionalAttendees){
-    if((Collections.disjoint(event.getAttendees(), request.getAttendees())) && (!hasOptionalAttendees || Collections.disjoint(event.getAttendees(), request.getOptionalAttendees()))){
-      return true;
-    }
-    return false;
+  private boolean checkIfAttendeesAreDisjoint(Event event, MeetingRequest request, boolean hasOptionalAttendees){
+    return Collections.disjoint(event.getAttendees(), request.getAttendees()) &&
+     (!hasOptionalAttendees ||
+       Collections.disjoint(event.getAttendees(), request.getOptionalAttendees()));
   }
 }
